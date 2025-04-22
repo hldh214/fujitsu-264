5cd5b88a52bfaa18c61e089018a67dd42aa99aff
hldh214 <hldh214@gmail.com>
2025-04-22 11:53:19 +0900

    Enhance logging in transmit_state for climate modes, fan modes, and swing settings in Fujitsu264 component

--- components/fujitsu_264/fujitsu_264.cpp
+++ components/fujitsu_264/fujitsu_264.cpp
@@ -55,22 +55,32 @@ void Fujitsu264Climate::transmit_state() {
   auto swing_mode = this->swing_mode;
   auto target_temp = this->target_temperature;
 
+  ESP_LOGD(TAG, "transmit_state() called");
+  ESP_LOGD(TAG, "mode: %d, fan_mode: %d, swing_mode: %d, target_temp: %.1f", mode, fan_mode, swing_mode, target_temp);
+
   // Configure AC state
   if (mode == climate::CLIMATE_MODE_OFF) {
+    ESP_LOGD(TAG, "Setting AC OFF");
     this->ac_.off();
   } else {
+    ESP_LOGD(TAG, "Setting AC ON");
     this->ac_.on();
 
     // Set mode
     if (mode == climate::CLIMATE_MODE_AUTO) {
+      ESP_LOGD(TAG, "Set mode: AUTO");
       this->ac_.setMode(kFujitsuAc264ModeAuto);
     } else if (mode == climate::CLIMATE_MODE_HEAT) {
+      ESP_LOGD(TAG, "Set mode: HEAT");
       this->ac_.setMode(kFujitsuAc264ModeHeat);
     } else if (mode == climate::CLIMATE_MODE_COOL) {
+      ESP_LOGD(TAG, "Set mode: COOL");
       this->ac_.setMode(kFujitsuAc264ModeCool);
     } else if (mode == climate::CLIMATE_MODE_DRY) {
+      ESP_LOGD(TAG, "Set mode: DRY");
       this->ac_.setMode(kFujitsuAc264ModeDry);
     } else if (mode == climate::CLIMATE_MODE_FAN_ONLY) {
+      ESP_LOGD(TAG, "Set mode: FAN_ONLY");
       this->ac_.setMode(kFujitsuAc264ModeFan);
     } else {
       ESP_LOGW(TAG, "Unsupported climate mode: %d", mode);
@@ -78,18 +88,24 @@ void Fujitsu264Climate::transmit_state() {
     }
 
     // Set temperature
+    ESP_LOGD(TAG, "Set temperature: %.1f", target_temp);
     this->ac_.setTemp(target_temp);
 
     // Set fan mode
     if (fan_mode == climate::CLIMATE_FAN_AUTO) {
+      ESP_LOGD(TAG, "Set fan: AUTO");
       this->ac_.setFanSpeed(kFujitsuAc264FanSpeedAuto);
     } else if (fan_mode == climate::CLIMATE_FAN_LOW) {
+      ESP_LOGD(TAG, "Set fan: LOW");
       this->ac_.setFanSpeed(kFujitsuAc264FanSpeedLow);
     } else if (fan_mode == climate::CLIMATE_FAN_MEDIUM) {
+      ESP_LOGD(TAG, "Set fan: MEDIUM");
       this->ac_.setFanSpeed(kFujitsuAc264FanSpeedMed);
     } else if (fan_mode == climate::CLIMATE_FAN_HIGH) {
+      ESP_LOGD(TAG, "Set fan: HIGH");
       this->ac_.setFanSpeed(kFujitsuAc264FanSpeedHigh);
     } else if (fan_mode == climate::CLIMATE_FAN_QUIET) {
+      ESP_LOGD(TAG, "Set fan: QUIET");
       this->ac_.setFanSpeed(kFujitsuAc264FanSpeedQuiet);
     } else {
       ESP_LOGW(TAG, "Unsupported fan mode: %d", fan_mode);
@@ -97,33 +113,45 @@ void Fujitsu264Climate::transmit_state() {
     }
 
     // Set swing mode
+    ESP_LOGD(TAG, "Set swing: %s", swing_mode == climate::CLIMATE_SWING_VERTICAL ? "VERTICAL" : "OFF");
     this->ac_.setSwing(swing_mode == climate::CLIMATE_SWING_VERTICAL);
   }
 
   auto state = this->ac_.getRaw();
   auto nbytes = this->ac_.getStateLength();
 
+  ESP_LOGD(TAG, "Raw state length: %d", nbytes);
+  ESP_LOGD(TAG, "Raw state bytes:");
+  for (uint16_t i = 0; i < nbytes; i++) {
+    ESP_LOGD(TAG, "  state[%d] = 0x%02X", i, state[i]);
+  }
+
   auto transmit = this->transmitter_->transmit();
   auto *call_data = transmit.get_data();
   call_data->set_carrier_frequency(38000);  // 38kHz
 
+  ESP_LOGD(TAG, "Sending IR header: mark %u, space %u", kFujitsuAcHdrMark, kFujitsuAcHdrSpace);
   call_data->mark(kFujitsuAcHdrMark);
   call_data->space(kFujitsuAcHdrSpace);
 
   for (uint16_t i = 0; i < nbytes; i++) {
     for (int8_t j = 7; j >= 0; j--) {
       call_data->mark(kFujitsuAcBitMark);
-      if (state[i] & (1 << j)) {
+      bool bit = state[i] & (1 << j);
+      if (bit) {
         call_data->space(kFujitsuAcOneSpace);
       } else {
         call_data->space(kFujitsuAcZeroSpace);
       }
+      ESP_LOGVV(TAG, "Bit: %d (byte %d, bit %d) -> %s", bit, i, j, bit ? "ONE" : "ZERO");
     }
   }
 
+  ESP_LOGD(TAG, "Sending IR footer: mark %u, space %u", kFujitsuAcBitMark, kFujitsuAcMinGap);
   call_data->mark(kFujitsuAcBitMark);
   call_data->space(kFujitsuAcMinGap);
 
+  ESP_LOGD(TAG, "Performing IR transmit");
   transmit.perform();
 
   ESP_LOGD(TAG, "Fujitsu AC remote state sent");
