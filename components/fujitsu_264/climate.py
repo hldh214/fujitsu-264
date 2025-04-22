8cb8a4fccc49b0c2536550886eb518bf1ca1f1ca
hldh214 <hldh214@gmail.com>
2025-04-22 16:10:37 +0900

    Refactor Fujitsu 264 component to use new Climate API
    Replaces the deprecated ClimateIR API with the updated Climate API for improved compatibility and functionality. Refactored setup, control methods, and temperature management to align with the new API. Updated configuration schema to work with the Climate API.
    
    Signed-off-by: hldh214 <hldh214@gmail.com>

--- components/fujitsu_264/climate.py
+++ components/fujitsu_264/climate.py
@@ -1,21 +1,21 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import climate_ir
+from esphome.components import climate
 from esphome.const import CONF_ID
 
-AUTO_LOAD = ["climate_ir"]
+AUTO_LOAD = ["climate"]
 
 fujitsu_264_ns = cg.esphome_ns.namespace('fujitsu_264')
-Fujitsu264Climate = fujitsu_264_ns.class_('Fujitsu264Climate', climate_ir.ClimateIR)
+Fujitsu264Climate = fujitsu_264_ns.class_('Fujitsu264Climate', climate.Climate)
 
-CONFIG_SCHEMA = climate_ir.CLIMATE_IR_SCHEMA.extend({
+CONFIG_SCHEMA = climate.CLIMATE_SCHEMA.extend({
     cv.GenerateID(): cv.declare_id(Fujitsu264Climate),
 })
 
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
-    await climate_ir.register_climate_ir(var, config)
+    await climate.register_climate(var, config)
     cg.add_library(
         name="IRremoteESP8266",
         repository="https://github.com/shimmy-void/IRremoteESP8266.git#bce5c0b0eeda8431a95d95c9947e4008bb0b982f",
